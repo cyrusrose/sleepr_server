6370ef72263861b958fc0833d49b495f33585360
Cyril Florence <cyrilwaremail@gmail.com>
2024-02-19 02:00:46 +0500

    Add common auth guard

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
-import { Request } from 'express'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { TokenPayload } from '../interfaces/token-payload.interface'
 import { UsersService } from '../users/users.service'
@@ -14,12 +13,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request?.cookies?.Authentication
+        (request: any) =>
+          request?.cookies?.Authentication || request?.Authentication
       ]),
       secretOrKey: configService.get('JWT_SECRET')
     })
   }
 
+  // adds user property to a request or payload
   async validate({ userId }: TokenPayload) {
     return this.usersService.getUser({ _id: userId })
   }
