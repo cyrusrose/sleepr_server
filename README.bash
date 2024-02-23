4a9035a9e640045366d5d26cf25f8d921bfcf5c8
Cyril Florence <cyrilwaremail@gmail.com>
2024-02-24 02:32:40 +0500

    Add K8s

--- README.bash
+++ README.bash
@@ -1,8 +1,28 @@
 # launching mongodb
 docker run --name mongodb -p '27017:27017' -v mongodb_data:/bitnami/mongodb bitnami/mongodb:5.0
 
-# build reservations
-docker build . -f ./apps/reservations/Dockerfile -t sleepr_reservations
+# build for production
+docker build . -f ./apps/reservations/Dockerfile -t reservations
+docker build . -f ./apps/auth/Dockerfile -t auth
+docker build . -f ./apps/notifications/Dockerfile -t notifications
+docker build . -f ./apps/payments/Dockerfile -t payments
 
-# compose
+# docker compose
 docker compose up -V
+
+# k8s
+kubectl create deployment reservations --image=reservations --dry-run=client -o yaml > deployment.yaml
+
+kubectl create secret generic mongodb --from-literal=MONGODB_URI=mongodb://mongo:27017/sleepr --dry-run=client -o yaml > mongodb.yaml
+
+kubectl apply -f ./secrets/mongodb.yaml
+
+kubectl create configmap reservations --from-file ../apps/reservations/.env --dry-run=client -o yaml > ./sleepr/templates/reservations/configmap.yaml
+
+kubectl logs auth-deployment-65d4dc646c-jvtwk
+
+kubectl get po --watch
+
+helm install sleepr .
+
+helm upgrade sleepr .
